baf726c20aaab0bdfd7abd7a7cd2903536f1fccb
Rotem Carr <155629245+RotemCarr@users.noreply.github.com>
2024-09-27 22:57:45 +0300

    made weight vector a random property now it is initialized randomly instead of being part of the constructor

--- components/neuron.py
+++ components/neuron.py
@@ -1,27 +1,23 @@
+import random
+
 from utils.activation_functions import *
 import numpy as np
 
 
 class Neuron:
 
-    def __init__(self, weight: float, bias: float):
+    def __init__(self, bias: float):
         self.activation = None
-        self.weight = weight
         self.bias = bias
         self.connections = None
 
     def __repr__(self):
-        return f"Neuron(activation: {self.activation}, weight: {self.weight}, bias: {self.bias})"
-
-    # def activate(self, layer):
-    #     activation_vector = layer.activation_vector
-    #     weight_matrix = layer.weight_matrix
-    #     self.activation = sigmoid(np.matmul(weight_matrix, activation_vector) + layer.bias_vector)
+        return f"Neuron(activation: {self.activation}, bias: {self.bias})"
 
     @property
     def weight_vector(self):
         if self.connections is not None:
-            return np.array([neuron.weight for neuron in self.connections])
+            return np.array([random.uniform(-1, 1) for _ in self.connections])
 
         return None
 
@@ -29,8 +25,8 @@ class Neuron:
 class InputNeuron(Neuron):
 
     def __repr__(self):
-        return f"Input(activation: {self.activation}, weight: {self.weight})"
+        return f"Input(activation: {self.activation})"
 
-    def __init__(self, input_activation, weight):
-        super().__init__(weight=weight, bias=0)
+    def __init__(self, input_activation):
+        super().__init__(bias=0)
         self.activation = input_activation
